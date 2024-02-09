7bf662f4f5a2473f89c39bc138fbe11e55463c39
romanStudent <132225725+romanStudent@users.noreply.github.com>
2024-02-09 09:27:51 +0100

    Update EditNote.js

--- src/components/EditNote/EditNote.js
+++ src/components/EditNote/EditNote.js
@@ -12,9 +12,26 @@ import ListOfExercises from './ListOfExercises';
 
 function EditNote({date, setDate, id_copy, save_edited, index_day, setIndexDay, days, setDays, amount_days, setAmountDays, show_edit_day, setShowEditDay , posts, setPosts, index, setIndex, amount_exercises, setAmountExercises}) {
 
+console.log(Calendar.propTypes);
+
+
+let url = `http://localhost:3003/plans/${id_copy}`;
+
+/*
+fetch(url) 
+  .then(response => response.json)
+  .then(main => JSON.parse(main.date))
+  .then(final => setDate(final));
+*/
+
+
+
   const ref = useRef();
  const [textareaCreate, setTextareaCreate] = useState('');
   const [textareaEdit, setTextareaEdit] = useState('');
+//   const [date, setDate] = useState(new Date());
+
+
   const [showTextareaCreate, setShowTextareaCreate] = useState('none');
   const [display_calendar, setDisplayCalendar] = useState('none');
 
@@ -28,6 +45,24 @@ function showChange() {
     return showTextareaCreate; 
 }
 
+/*
+let url = `http://localhost:3003/plans/${id_copy}`;
+
+if(id_copy >= 0) {
+async function save_date() {
+await fetch(url)
+            .then(response => response.json())
+        .then(main => setmain.date)
+}
+save_date();
+}
+*/
+
+   console.log('DAY ID EDITNOTE' + id_copy);
+
+
+
+
                 let post = {
           index: index,
           textarea: textareaCreate,
@@ -40,65 +75,132 @@ function showChange() {
     date: date
    }
 
+//let day_str = day.date.getFullYear() + '/' + day.date.getMonth() + '/' + day.date.getDate();
 
 function save() {
+
+
+console.log(days);
+console.log(days[id_copy].amount_exercises);
 setAmountExercises(posts.length);
 
-   days[id_copy] = day;
+   console.log(amount_exercises);
 
+   // setDays([...days, day]);
+   // setDays(days.splice())
+   console.log(days[id_copy]);
+   days[id_copy] = day;
+   console.log(days[id_copy]);
+
+
+
+
+
+
+// let plans = 'http://localhost:3003/plans';
+//   let plans = 'https://v1.nocodeapi.com/programmer_roman/netlify/hTWTzglhPRxhhqjZ/listForms?site_id=062b08cf-49c4-415a-9a90-b397c72e143f';
 
 let fin = day.date;
 
   async function send() {
-let cop = day.date;
-    day.date = cop;
- 
-    let sendParams = new URLSearchParams();
-          sendParams.set('id', Number(day.id));
-          sendParams.set('amount', Number(day.amount_exercises));
-          sendParams.set('posts', JSON.stringify(posts));
-          sendParams.set('date', JSON.stringify(day.date));
 
+// УРА НАХУЙ!!! ДАННЫЕ В JSON ЗАМЕНЯЮТСЯ
 
-cop.setDate(cop.getDate() - 1);
-day.date = cop;
+   console.log(id_copy);    // НАКОНЕЦ-ТО НАХУЙ. ЕБУЧИЙ ПРАВИЛЬНЫЙ 'ID' ПОЛУЧИЛ
+
+/*
+let h = cop.setDate(cop.getDate());
+
+setDate(h);
+*/
+// cop.setDate(cop.getDate() - 1);
+// day.date = cop;
 
 const ypo = {
-    id: day.id,
-    amount_exercises: day.amount_exercises,
-    posts: posts,
-    date: day.date
+    id: Number(day.id),
+    amount_exercises: Number(day.amount_exercises),
+    posts: JSON.stringify(posts),
+    date: JSON.stringify(day.date)
 }
 
 
+/*
+await fetch(url, {
+    method: 'PUT',
+    body: sendParams
+})
+*/
 
 
 async function putData(url = "", data = {}) {
+    console.log(data);
+    console.log(url);
     console.log('sd');
   // Default options are marked with *
+    /*
   const response = await fetch(url, {
     method: "PUT", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+   // mode: "cors", // no-cors, *cors, same-origin
+   // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   // credentials: "same-origin", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+   // redirect: "manual", // manual, *follow, error
+   // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response; // parses JSON response into native JavaScript objects
+  */
+/*
+    fetch(url, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+   // mode: "cors", // no-cors, *cors, same-origin
+   // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   // credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "content-type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+   // redirect: "manual", // manual, *follow, error
+   // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+*/
+
+
+////////////////////// ТУТ ПОМЕНЯТЬ КОД ДЛЯ МЕТОДА "PUT", А НЕ "GET"
+    /*
+       const response = await axios.put(url);
+       const result = response.data;
+       
+       if(result[ind] == undefined) {
+         edit_day_func(id);
+       } else {
+        const resultFinal = JSON.parse(result[ind].posts);
+        setPosts(resultFinal);
+        const dateFinal = result[ind].date;
+        let v = dateFinal; let str_w = v.split('T')[0]; setDate(str_w);
+       }
+       // const resultFinal = JSON.parse(result[0].posts);
+       
+       console.log(ind);
+       console.log(result[ind]);
+    */
 }
 
 
-
-putData(`${window.location.origin}/api/${day.id}`, ypo).then((data) => {
+/*
+putData(`http://localhost:3012/api/${id_copy}`, ypo).then((data) => {
+    console.log(id_copy);
   console.log(data); // JSON data parsed by `data.json()` call
   console.log('sd');
 })
-.catch((err) => console.log(err));
+.catch((err) => {console.log(err.message); console.log(ypo)});
+*/
+
+setPosts([]);
 }
 
 send();
@@ -129,11 +231,13 @@ function deletePost(post) {
     setIndex(posts.length - 1);
 }
 
+/*
 if(display_calendar == 'block') {
 document.getElementsByClassName('react-calendar__viewContainer')[1].addEventListener('click', function() {
    setDisplayCalendar('none');
 })
 }
+*/
 
 
 function memo() {
@@ -149,7 +253,15 @@ function date_func() {
   return (
     <div className="edit_note_block text-center md:col-span-6 sm:col-span-12 border border-2 border-white rounded-xl" style={{/*display: show_edit_day*/ display:'block'}}>
        <h3 className="header text-xl font-bold">Edit Note</h3>
+         {/*
+         <div className="dropdown body">
+            <input type="button" className="btn btn-primary bg-primary dropdown-toggle w-40 h-20 text-xl font-bold show" data-bs-toggle="dropdown" placeholder="Select Date" onChange={setDate} value={date.toLocaleString("ru", {day: 'numeric', month: 'numeric', year: 'numeric'})} />
 
+            <div className="dropdown-menu text-center border border-2 border-white rounded-xl">
+                <Calendar onChange={setDate} value={date} defaultValue={new Date()} minDate={new Date()} className="calendar" dateFormat="dd/mm/yy" />
+            </div>
+         </div>
+         */}
 
        <div className="calendar_main">
              <input type="button" className="btn w-40 h-20 text-xl font-bold mt-5" placeholder="Select Date" onChange={setDate} onClick={date_func} value={date.toLocaleString("ru", {day: 'numeric', month: 'numeric', year: 'numeric'})} />
@@ -164,7 +276,7 @@ function date_func() {
 
          <div className="body mt-20">
 
-           {memo()}
+           {/* useMemo(() => {memo()}, [posts]) */ memo()}
                  
           
 
@@ -181,6 +293,7 @@ function date_func() {
                        p-3
                        m-2" 
                     onClick={showChange}
+                           //     onClick={add}
 
          />
          

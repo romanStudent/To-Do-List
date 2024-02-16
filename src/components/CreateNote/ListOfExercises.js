cfb036cb31cbb74ee8a3d7c733a35838a4a2e6bf
romanStudent <132225725+romanStudent@users.noreply.github.com>
2024-02-16 23:20:25 +0100

    Update ListOfExercises.js

--- src/components/CreateNote/ListOfExercises.js
+++ src/components/CreateNote/ListOfExercises.js
@@ -5,105 +5,74 @@ import './ListOfExercises.css';
 
 function ListOfExercises({ref, amount_exercises, setAmountExercises, deletePost, deleteText, posts, setPosts, index, setIndex, textareaCreate, setTextareaCreate, textareaEdit, setTextareaEdit, showTextareaCreate, setShowTextareaCreate}) {
 
-
-
-
-
-function transform(post) {
+  function transform(post) {
         let text_copy = post.textarea;
 
-// console.log(post);
-// console.log(posts);
+        if(window.innerWidth > 1250) {
+           
+           if(post.textarea.length > 34) {
+               text_copy = text_copy.slice(0, 31) + '...';
+           } else {
+               text_copy = post.textarea;
+           }
 
-      if(window.innerWidth > 1250) {
-        if(post.textarea.length > 34) {
+        } 
+        
+        else if(window.innerWidth >= 480) {
+           
+           if(post.textarea.length > 24) {
+               text_copy = text_copy.slice(0, 21) + '...';
+           } else {
+               text_copy = post.textarea;
+           }
 
-            text_copy = text_copy.slice(0, 31) + '...';
-           // console.log(post.textarea);
         } else {
-           text_copy = post.textarea;
+           if(post.textarea.length > 14) { 
+               text_copy = text_copy.slice(0, 11) + '...';
+           } else {
+               text_copy = post.textarea;
+           }
         }
 
-
-} else if(window.innerWidth >= 480) {
-    if(post.textarea.length > 24) {
-            
-         text_copy = text_copy.slice(0, 21) + '...';
-              // console.log(post.textarea);
-        } else {
-           text_copy = post.textarea;
-        }
-
-
-} else {
-    if(post.textarea.length > 14) {
-            
-            text_copy = text_copy.slice(0, 11) + '...';
-           // console.log(post.textarea);
-        } else {
-           text_copy = post.textarea;
-        }
-}
-
-return text_copy;
-}
-
+   return text_copy;
+  }
 
 
       function ChangeTextCreate(e) {
-
            setTextareaCreate(e.target.value);
-         //  console.log(post);
       }
 
-
-
-                 function ChangeTextEdit(e) {
-
+      function ChangeTextEdit(e) {
            setTextareaEdit(e.target.value);
-
       }
 
 
-                let post = {
+      let post = {
           index: index,
           textarea: textareaCreate,
           id: index
-        };
+      };
 
       function add() {
-        setIndex(index + 1);
-   
+           setIndex(index + 1);
+           setPosts([...posts, post]);
+           setTextareaCreate('');
+           setAmountExercises(posts.length + 1);
+      }
 
-        setPosts([...posts, post]);
-                // console.log(post.textarea);
-               // console.log(posts);
-                          
-                setTextareaCreate('');
-                // console.log(posts.length);
-        setAmountExercises(posts.length + 1);
+      function add_func() {
+           add();
+           setShowTextareaCreate('none');
       }
 
 
-
-        function add_func() {
-           // console.log('add');
-        add();
-        setShowTextareaCreate('none');
-    }
-
-
-                 function ChangeTextEditFirst(post) {
-
+      function ChangeTextEditFirst(post) {
            setTextareaEdit(post.textarea);
-
       }
-
-
 
 
     return (    
-      <div>     
+     <div>     
           <div className="exercises m-3">
             <h3 className="text-xl">Exercises</h3>
             
@@ -111,49 +80,38 @@ return text_copy;
                  { 
                     posts.map((post, id) => 
                     <ExerciseCard transform={transform} ref={ref} ChangeTextEditFirst={ChangeTextEditFirst} ChangeTextEdit={ChangeTextEdit} deletePost={deletePost} setPosts={setPosts} add_func={add_func} posts={posts} post={post} key={id} id={id} textareaCreate={textareaCreate} setTextareaCreate={setTextareaCreate} textareaEdit={textareaEdit} setTextareaEdit={setTextareaEdit} showTextareaCreate={showTextareaCreate} setShowTextareaCreate={setShowTextareaCreate} />
-)}
+                 )}
               </div>
 
           </div>
 
 
+          <div className="textareaCreate text-center" style={{display: showTextareaCreate}}>
 
-    <div className="textareaCreate text-center" style={{display: showTextareaCreate}}>
-                   
                    <h4 className="m-2 text-lg">Write your Exercise</h4>
                    <textarea className="description border border-2 border-black rounded-xl h-20 mr-1 ml-1" 
                              rows="4" 
                              cols="50" 
                              onChange={ChangeTextCreate} 
-                             value={textareaCreate}>
-                                
-                    </textarea>
+                             value={textareaCreate}>                            
+                   </textarea>
         
-             <input 
-                    type="button" 
-                    name="Save" 
-                    value="Save exercise"
-                    className="bg-black 
-                               text-white 
-                               border border-2 border-black rounded-xl
-                               p-3
-                               mr-1
-                               ml-1" 
-                   // onClick={() => {return <NewExercise/>} }
-                                onClick={() => {add_func(); transform(post)}}
+                   <input 
+                        type="button" 
+                        name="Save" 
+                        value="Save exercise"
+                        className="bg-black 
+                                   text-white 
+                                   border border-2 border-black rounded-xl
+                                   p-3
+                                   mr-1
+                                   ml-1" 
+                        onClick={() => {add_func(); transform(post)}}
+                   />
 
-             />
-
-    </div>
-
-
-
-
-
-         </div>
-
-           )
-  
+          </div>
+     </div>
+    )
 }
 
 export default ListOfExercises;

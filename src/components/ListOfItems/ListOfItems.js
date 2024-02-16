4836793b1feba981bfc0f4fa7391a9471cccdc33
romanStudent <132225725+romanStudent@users.noreply.github.com>
2024-02-16 23:25:17 +0100

    Update ListOfItems.js

--- src/components/ListOfItems/ListOfItems.js
+++ src/components/ListOfItems/ListOfItems.js
@@ -4,32 +4,34 @@ import ReactDOM from 'react-dom/client';
 import Item from '../Item/Item';
 import App from '../../App';
 import CreateNote from '../CreateNote/CreateNote';
+
 import './ListOfItems.css';
-
-
 
 
 
 function ListOfItems({posts, edit_day_func, id_copy, setIdCopy, id_func, days, setPosts, index, setIndex}) {
 
   return (
-    <div className="list_of_items border border-2 border-white rounded-xl p-2 mb-10 list_and_button">
-        <h2 className="font-bold h2">MY LISTS</h2>
+    <div className="list_of_items border border-2 border-white rounded-xl h-max p-2 mr-1.5 ml-1.5 pb-5 md: mb-10 list_and_button">
+        <h1 className="font-bold h1 text-xl">MY LIST</h1>
          
          <ul className="dates m-2">
            {days.map((day, index) => 
-              <Item /*id={day.id}*/ id={index} key={index} id_copy={id_copy} setIdCopy={setIdCopy} id_func={id_func} edit_day_func={edit_day_func} day={day} posts={posts} />    
+              <Item id={index} key={index} id_copy={id_copy} setIdCopy={setIdCopy} id_func={id_func} edit_day_func={edit_day_func} day={day} posts={posts} />    
            )}
               
          </ul>
               <Link to="/create">
-                    <input type="button" name="Add" className="add_note 
-                                                     bg-black  
-                                                     text-white 
-                                                     border border-2 border-black rounded-xl
-                                                     p-3" 
-                                              value="Add" 
-                                              onClick={() => {setPosts([]); if(index !== undefined) {console.log(index); setIndex(0)}}}
+                    <input 
+                         type="button" 
+                         name="Add" 
+                         className="add_note 
+                                    bg-blue-500  
+                                    text-white 
+                                    border border-2 border-white rounded-xl
+                                    p-3" 
+                         value="Add" 
+                         onClick={() => {setPosts([]); if(index !== undefined) {setIndex(0)}}}
                     />
               </Link>
     </div>
